8197cc9cac1fcf98d9d748f43de86662ee50f5a0
LichRich <leech135789@gmail.com>
2023-12-04 16:17:47 +0900

    commit for modifying router

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css'
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <HashRouter>
         <App />
-      </HashRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
